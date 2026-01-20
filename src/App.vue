936b2bd6a9540f61cd2a25535b4e8e4398116feb
Mr_bl <1300264929@qq.com>
2026-01-20 18:31:01 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import Elem from './view/Elem.vue'
 </script>
 
 <template>
-
+  <div>
+    <Elem />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/view/Elem.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { ElButton } from 'element-plus'
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+const tableData = ref([])
+
+
+
+</script>
+
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+  <el-backtop :right="100" :bottom="100" />
+</template>
+
+<style scoped>
+
+</style>
